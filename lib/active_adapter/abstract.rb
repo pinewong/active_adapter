4e7621acb2bf42bc9e9190aa4a0873232f3bf6fb
Pine Wong <pinewong@163.com>
2018-07-24 18:30:47 +0800

    Add doc for engine and abstract class

--- lib/active_adapter/abstract.rb
+++ lib/active_adapter/abstract.rb
@@ -2,6 +2,8 @@ module ActiveAdapter
   class Abstract
     attr_reader :adapter
 
+    # Instance an real object from abstract object by adapter
+    # @param implement_adapter_class [Class] The implement adapter class
     def initialize(implement_adapter_class)
       @adapter = implement_adapter_class.new
     end
--- lib/active_adapter/engine.rb
+++ lib/active_adapter/engine.rb
@@ -5,6 +5,33 @@ module ActiveAdapter
         @adapters ||= {}
       end
 
+      # To adapt engine object for a real object
+      # @param adapter [#to_sym] Adapter class name
+      # @return [Object] The real object with a adapter
+      # @raise [ActiveAdapter::AbstractAdapterUndefinedError] if there is no *_adapters folder or
+      #   there is no abstract class(inherit from ActiveAdapter::Abstract) in *_adapters folder
+      # @raise [ActiveAdapter::ImplementAdapterUndefinedError] if there is no implement class
+      #   inherit from ActiveAdapter::Implement)
+      # @example
+      #   # vehicle.rb
+      #   class Vehicle < ActiveAdapter::Engine; end
+      #
+      #   # vehicle_adapters/abstract.rb
+      #   class VehicleAdapters::Abstract < ActiveAdapter::Abstract
+      #     def run
+      #       adapter.run
+      #     end
+      #   end
+      #
+      #   # vehicle_adapters/car.rb
+      #   class VehicleAdapters::Car < ActiveAdapter::Implement
+      #     def run
+      #       "Car running..."
+      #     end
+      #   end
+      #
+      #   vehicle = Vehicle.adapt(:car)
+      #   vehicle.run # => "Car running..."
       def adapt(adapter)
         adapters[adapter.to_s] ||= begin
           module_name = "#{name.deconstantize}::#{name.demodulize}Adapters"
